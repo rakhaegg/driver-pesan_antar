c0770ab03d954bcd64bbecdce5e3cdca7594a1d2
rakhaegg <43262815+rakhaegg@users.noreply.github.com>
2021-12-29 09:20:33 +0700

    Update DriversService.js

--- src/services/DriversService.js
+++ src/services/DriversService.js
@@ -14,6 +14,7 @@ class DriverService {
             host: process.env.DB_HOST,
             user: process.env.DB_USERNAME,
             database: process.env.DB_DATABASE,
+            password : process.env.DB_PASSWORD,
             port: 3306
         });
     }
