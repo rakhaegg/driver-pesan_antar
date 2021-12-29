ab3db184b10cd1f4ef5a45b453dbd3ffb01b0b8e
rakhaegg <43262815+rakhaegg@users.noreply.github.com>
2021-12-29 09:21:10 +0700

    Update AuthenticationsService.js

--- src/services/AuthenticationsService.js
+++ src/services/AuthenticationsService.js
@@ -7,6 +7,7 @@ class AuthenticationsService {
       host: process.env.DB_HOST,
       user: process.env.DB_USERNAME,
       database: process.env.DB_DATABASE,
+      password : process.env.DB_PASSWORD,
       port: 3306
     });
   }
